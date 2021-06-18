c1e4301cbd6ce5cbfe01c858ad199cd17f17eb32
niuniuanran <irene.anran.niu@gmail.com>
2021-06-18 20:04:09 +1200

    Add api logic

---
+++ src/components/email-block/logic/api.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const POST = "POST";
+
+const config = {
+  recipeUrl: "https://api.buildable.dev/trigger",
+  recipeEnv: process.env.NODE_ENV === "development" ? "test" : "live",
+  version: "v2"
+};
+
+const { recipeUrl, recipeEnv, version } = config;
+
+const api = async ({ payload, url, headers = {} }) => {
+  const { data } = await axios({
+    method: POST,
+    url,
+    headers,
+    data: payload
+  });
+
+  return data;
+};
+
+const dispatchRecipe = ({ triggerId, payload = {}, options = {} }) =>
+  api({
+    ...options,
+    url: `${recipeUrl}/${version}/${recipeEnv}-${triggerId}`,
+    payload
+  });
+
+export default dispatchRecipe;
