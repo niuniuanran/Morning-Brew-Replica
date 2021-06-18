731d1b5f81503971637f9155bb8ca797bb3c67df
niuniuanran <irene.anran.niu@gmail.com>
2021-06-18 20:07:05 +1200

    Update get-content logic

--- src/components/email-block/logic/data-models.js
+++ src/components/email-block/logic/data-models.js
@@ -1,11 +1,25 @@
 import { content } from "../fallback";
+import dispatchRecipe from "./api";
+
+const RECIPES = {
+  LEADS_BLOCK: "ab4745dc-935e-4ac5-bf45-73d82e3241d7"
+};
 
 const initContentModel = (set) => ({
   content,
   currentButtonText: content.button?.states?.initial,
   setButtonText: (buttonText) =>
     set((state) => ({ ...state, currentButtonText: buttonText })),
-  getContent: () => {},
+    getContent: async () => {
+      const payload = {
+        key: "content"
+      };
+      const content = await dispatchRecipe({
+        triggerId: RECIPES.LEADS_BLOCK,
+        payload
+      });
+      set((state) => ({ ...state, content }));
+    },
   setContent: (content) => {
     set((state) => ({ ...state, content }));
   }
